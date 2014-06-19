5778e8e8b5359770acfa0faa6914eaeaeed3108e
Erwann Mest <m@kud.io>
2014-06-19 19:45:17 +0200

    Fix when content is empty.

--- data/script.js
+++ data/script.js
@@ -1,6 +1,7 @@
 var el = document.getElementById('content')
 
 self.port.on('content', function( content ) {
+  if ( content === null || content === '' || content === '<br>' ) content = 'Insert your text here.'
   el.innerHTML = content
 
   document.addEventListener('keyup', function() {
